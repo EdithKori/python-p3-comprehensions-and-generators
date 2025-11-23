adb8159faf44e5b73a2df30c7195923892be876e
Joy Kori <joyedithkori@gmail.com>
2025-11-23 16:05:17 +0300

    finishes list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+# lib/list_comprehension.py
+
+def return_evens(int_list):
+    """
+    Returns a list of all even numbers in int_list using a list comprehension.
+    """
+    return [num for num in int_list if num % 2 == 0]
+
+
+def make_exclamation(sentences):
+    """
+    Returns a new list where each sentence in the input list has an exclamation mark added at the end.
+    """
+    return [sentence + "!" for sentence in sentences]
